0c441ef015e7536d344af49353b6f83b557e8e4d
Aaimry <wheal28@gmail.com>
2022-04-02 12:32:05 +0600

    создана модель фотографии и альбома,зарегистрировала в админке, создала и применила  миграцию

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from webapp.models import Photo, Album
 
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'photo', 'description', 'author', 'create_date', 'is_private', 'album']
+    fields = ['photo', 'description', 'author', 'is_private', 'album']
+    readonly_fields = ['id', 'create_date']
+
+
+admin.site.register(Photo, PhotoAdmin)
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'author', 'is_private', 'create_date']
+    fields = ['title', 'description', 'author', 'is_private']
+    readonly_fields = ['id', 'create_date']
+
+
+admin.site.register(Album, AlbumAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='photos', blank=False, null=False, verbose_name='Фото')
+    description = models.CharField(max_length=100, blank=False, null=False, verbose_name='Описание')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='author', verbose_name='Автор')
+    is_private = models.BooleanField(default=False, verbose_name='Приватность')
+    album = models.ForeignKey('webapp.Album', on_delete=models.CASCADE, related_name="album", null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.description} {self.author}'
+
+    class Meta:
+        db_table = 'Photos'
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False, verbose_name='Название')
+    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name='Описание')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='album_author',
+                                     verbose_name='Автор Альбома')
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_private = models.BooleanField(default=False, verbose_name='Приватность')
+
+    def __str__(self):
+        return f'{self.title}{self.description} {self.author}'
+
+    class Meta:
+        db_table = 'Albums'
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбомы'
